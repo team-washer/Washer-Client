1a447fd1e5b6a3243b4194cd717056bc3b7bd73c
민우석 <s23044@gsm.hs.kr>
2025-07-16 20:44:24 +0900

    Change file route

---
+++ app/api/auth/signin/route.ts
@@ -0,0 +1,61 @@
+import { AuthResponse } from '@/lib/api-client';
+import { apiClient } from '@/lib/api-request';
+import RoleEncryption from '@/lib/role-encryption';
+import { NextRequest, NextResponse } from 'next/server';
+
+interface RequestBody {
+  email: string;
+  password: string;
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const body: RequestBody = await request.json();
+
+    const response: AuthResponse = await apiClient.post('/auth/signin', body);
+    console.log(response.data.data);
+
+    if (response.data && response.data.success && response.data.data) {
+      const {
+        accessToken,
+        accessTokenExpiredAt,
+        refreshToken,
+        refreshTokenExpiredAt,
+        role,
+      } = response.data.data;
+
+      const accessTokenExpires = new Date(`${accessTokenExpiredAt}+09:00`);
+      const refreshTokenExpires = new Date(`${refreshTokenExpiredAt}+09:00`);
+
+      const res = NextResponse.json(response.data.data);
+
+      res.cookies.set('accessToken', accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        expires: accessTokenExpires,
+        sameSite: 'strict',
+      });
+
+      res.cookies.set('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        expires: refreshTokenExpires,
+        sameSite: 'strict',
+      });
+
+      res.cookies.set('role', RoleEncryption({role}), {
+        httpOnly: false,
+        secure: process.env.NODE_ENV === 'production',
+        expires: accessTokenExpires,
+        sameSite: 'strict',
+      });
+
+      return res;
+    }
+  } catch (err) {
+    return NextResponse.json(
+      { error: '서버에서 토큰 정보를 받아오지 못 했습니다.' },
+      { status: 500 }
+    );
+  }
+}
