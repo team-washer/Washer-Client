9e4ba154fc7877617e9eab3be5cff98ac1d184ae
민우석 <s23044@gsm.hs.kr>
2025-07-17 01:39:39 +0900

    Add reissue api

---
+++ src/app/api/auth/reissue/route.ts
@@ -0,0 +1,54 @@
+import { apiClient } from '@/shared/lib/api-request';
+import RoleEncryption from '@/shared/lib/role-encryption';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  const refreshToken = request.cookies.get('refreshToken')?.value;
+
+  try {
+    const response = await apiClient.post('/auth/reissue', {
+      'Refresh-Token': `Bearer ${refreshToken}`,
+    });
+    if (response.data && response.data.success && response.data.data) {
+      const {
+        accessToken,
+        accessTokenExpiredAt,
+        refreshToken,
+        refreshTokenExpiredAt,
+        role,
+      } = response.data.data;
+
+      const accessTokenExpires = new Date(`${accessTokenExpiredAt}+09:00`);
+      const refreshTokenExpires = new Date(`${refreshTokenExpiredAt}+09:00`);
+
+      const res = NextResponse.json(response.data.data);
+
+      res.cookies.set('accessToken', accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        expires: accessTokenExpires,
+        sameSite: 'strict',
+      });
+
+      res.cookies.set('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        expires: refreshTokenExpires,
+        sameSite: 'strict',
+      });
+
+      res.cookies.set('role', RoleEncryption({ role }), {
+        httpOnly: false,
+        secure: process.env.NODE_ENV === 'production',
+        expires: accessTokenExpires,
+        sameSite: 'strict',
+      });
+      return res;
+    }
+  } catch (err) {
+    return NextResponse.json(
+      { error: '서버에서 토큰 정보를 받아오지 못 했습니다.' },
+      { status: 500 }
+    );
+  }
+}
